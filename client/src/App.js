21a7c221260afdbd0097b87b53ad1c71efd92e77
Magno Raymundo Diaz <106284472+MagnoRay@users.noreply.github.com>
2025-11-12 02:51:20 +0000

    lema

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HuarazSky, el poder de los andes, en la nube</h1>
+      <h1>Warasky, el poder de los andes, en la nube</h1>
     </div>
   );
 }
